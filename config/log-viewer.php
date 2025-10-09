8728a1d5927e2f5f9d323502bc82e147b5803b3a
kikorobledo <enrique_j_@hotmail.com>
2025-10-09 15:39:24 -0600

    Fix provider

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -74,7 +74,6 @@ return [
     'middleware' => [
         'web',
         'auth',
-        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     /*
@@ -87,8 +86,8 @@ return [
     */
 
     'api_middleware' => [
+        'auth',
         \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     'api_stateful_domains' => env('LOG_VIEWER_API_STATEFUL_DOMAINS') ? explode(',', env('LOG_VIEWER_API_STATEFUL_DOMAINS')) : null,
